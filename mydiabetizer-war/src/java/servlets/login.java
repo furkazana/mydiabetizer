ea9b0930fcd53dc0d30d6f863081e92a5b211837
tsvetelintsonev <tsvetelin.tsonev@yahoo.co.uk>
2016-02-06 01:00:06 +0100

    login modified

--- mydiabetizer-war/src/java/servlets/login.java
+++ mydiabetizer-war/src/java/servlets/login.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package servlets; 
+package servlets;
 
 import beans.userBeanLocal;
 import controllers.EncryptionControl;
@@ -14,6 +14,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Date;
 import javax.ejb.EJB;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -24,65 +25,56 @@ import javax.servlet.http.HttpServletResponse;
  * @author Rock n Roll
  */
 public class login extends HttpServlet {
+
     @EJB
     private userBeanLocal userBean;
 
-    
-
-  
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException 
-    {
-       
+            throws ServletException, IOException {
+
     }
 
-  
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException 
-    {
-       String email= request.getParameter("email");
-       String password= request.getParameter("password");
-       
-       Date date;
-       date=userBean.getDate(email);
-       String  hexPassword=null;
-       
-       if(date!=null)
-       {
-       EncryptionControl encr =new EncrAlgs(password);
-       encr.check(date);
-       hexPassword=encr.getHexPassword();
-       }
-       
-       boolean bo=userBean.isValidUsr(email,hexPassword);
-       
-          
-       
-       Diabetics d = userBean.getDiabetic();
-        String sirname=d.getSurname();
-                   try (PrintWriter out = response.getWriter()) 
-                    {/* 
-                    out.println("<!DOCTYPE html>");
-                    out.println("<html>");
-                    out.println("<head>");
-                    out.println("<title>Servlet login</title>");            
-                    out.println("</head>");
-                    out.println("<body>");
-                 
-                    out.println("<h1>Error</h1>");
-                    out.println("<h1>user is:"+d.getEmail()+" password is :"+d.getPassword()+"valid user :"+" </h1>");
-                    out.println("<h1>"+"first name to go here" +" </h1>");
-                    out.println("</body>");
-                    out.println("</html>");
-                 */  
-           request.setAttribute("SIRNAME",sirname);
-           this.getServletContext().getRequestDispatcher("/mypage.jsp").forward(request, response);
-    }
+            throws ServletException, IOException {
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+
+        Date date;
+        date = userBean.getDate(email);
+        String hexPassword = null;
+
+        if (date != null) {
+            EncryptionControl encr = new EncrAlgs(password);
+            encr.check(date);
+            hexPassword = encr.getHexPassword();
+        }
+
+        boolean bo = userBean.isValidUsr(email, hexPassword);
+
+        Diabetics d = userBean.getDiabetic();
+        String sirname = d.getSurname();
+//        try (PrintWriter out = response.getWriter()) {/* 
+//             out.println("<!DOCTYPE html>");
+//             out.println("<html>");
+//             out.println("<head>");
+//             out.println("<title>Servlet login</title>");            
+//             out.println("</head>");
+//             out.println("<body>");
+//                 
+//             out.println("<h1>Error</h1>");
+//             out.println("<h1>user is:"+d.getEmail()+" password is :"+d.getPassword()+"valid user :"+" </h1>");
+//             out.println("<h1>"+"first name to go here" +" </h1>");
+//             out.println("</body>");
+//             out.println("</html>");
+//             */
+//
+//            //request.setAttribute("SIRNAME",sirname);
+//        }
+        response.sendRedirect(request.getContextPath() + "/mypage.jsp");
     }
 
-   
     @Override
     public String getServletInfo() {
         return "Short description";
