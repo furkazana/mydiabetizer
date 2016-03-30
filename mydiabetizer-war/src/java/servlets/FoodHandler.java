2679dd85c77fb7da6b70dc967d0640f1a989d09b
Rock n Roll <Rock n Roll@RocknRoll>
2016-03-30 11:56:39 +0100

    ....

--- mydiabetizer-war/src/java/servlets/FoodHandler.java
+++ mydiabetizer-war/src/java/servlets/FoodHandler.java
@@ -8,6 +8,7 @@ package servlets;
 import beans.FoodHandlerBeanLocal;
 import beans.diaryDatesBean;
 import ent.Beverages;
+import ent.ChartEntity;
 import ent.Fatsandsweets;
 import ent.Fruits;
 import ent.Meats;
@@ -15,8 +16,11 @@ import ent.Starches;
 import ent.Vegetables;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.ParseException;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.EJB;
 import javax.json.Json;
 import javax.json.JsonArray;
@@ -29,6 +33,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import statics.CarbsHandler;
+import statics.Diary;
 import statics.Insulin;
 //import com.google.gson.Gson;
 
@@ -69,106 +74,118 @@ public class FoodHandler extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        JsonReader jr = Json.createReader(request.getInputStream());
-        JsonObject jo = jr.readObject();
-        String mealData = jo.getJsonString("mealData").getString();
-        String ill = jo.getJsonString("ill").getString();
-        String activity = jo.getJsonString("activity").getString();
-        String mealType = jo.getJsonString("mealType").getString();
-        String bloodSugar = jo.getJsonString("bloodSugar").getString();
-       
-        CarbsHandler ch = new CarbsHandler();
-        String carbs = ch.Run(mealData, ill, activity);
-        
-        double bs=0.0;
-        
         try {
-         
-               bs=Double.parseDouble(bloodSugar);
-            } 
-        catch(NumberFormatException e)
-        {
-
-        }      
-        
-        
-       
-//        Insulin in=new Insulin();
-//        
-//        int ins=in.InsulinUnitsCalculation(3, Integer.parseInt(carbs),bs);
-        
-        
-     //   response.setContentType("application/json");
-        JsonObjectBuilder jsonObjBuilder = Json.createObjectBuilder();
-        JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
-        JsonArray ja;
-        
-        arrayBuilder.add(jsonObjBuilder.add("date", "1256-45-35").add("value", 5)).add(jsonObjBuilder.add("date", "1256-45asdasd").add("value", 10));
-        
-        ja = arrayBuilder.build();
-        
-        JsonObject jsonObj = jsonObjBuilder.add("array", ja).add("insulin", 10).build();
-        
-        
-        try (PrintWriter out = response.getWriter()) {
-             out.println(jsonObj);
-        }
-//        
+            
+            JsonReader jr = Json.createReader(request.getInputStream());
+            JsonObject jo = jr.readObject();
+            String mealData = jo.getJsonString("mealData").getString();
+            String ill = jo.getJsonString("ill").getString();
+            String activity = jo.getJsonString("activity").getString();
+            String mealType = jo.getJsonString("mealType").getString();
+            String bloodSugar = jo.getJsonString("bloodSugar").getString();
+            
+            CarbsHandler ch = new CarbsHandler();
+            String carbs = ch.Run(mealData, ill, activity);
+            
+            double bs=0.0;
+            
+            try {
+                
+                bs=Double.parseDouble(bloodSugar);
+            }
+            catch(NumberFormatException e)
+            {
+                
+            }
+            
+            
+            
+            Insulin in=new Insulin();
+            
+            int ins=in.InsulinUnitsCalculation(3, Integer.parseInt(carbs),bs);
+            List<ChartEntity> Charlist= diaryDatesBean.test(2);
+            Diary d=new Diary();
+            
+            
+            response.setContentType("application/json");
+            JsonObjectBuilder jsonObjBuilder = Json.createObjectBuilder();
+            JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
+            JsonArray ja;
+            
+            arrayBuilder.add(jsonObjBuilder.add("date", "1256-45-35").add("value", 5)).add(jsonObjBuilder.add("date", "1256-45asdasd").add("value", 10));
+            
+            ja = arrayBuilder.build();
+            
+            JsonObject jsonObj = d.getJsonCustom(Charlist);
+            
+            
+            
+            
+            try (PrintWriter out = response.getWriter()) {
+                out.println(jsonObj);
+            }
+//
 //        Map<String, Map<String, String>> meals = new HashMap<String, Map<String, String>>();
 //        Map<String, String> foodCat = new HashMap<String, String>();
 //        foodCat.put("potatoes", "Vegetablees");
 //        foodCat.put("tomatoes", "Vegetablees");
 //        foodCat.put("cucumber", "Vegetablees");
 //        meals.put("moussaka", foodCat);
-        
-        
+
+
 //         d.addToDiary(currentSugar,10+"","", ill, 2, mealType);
-                 //       request.setAttribute("test", activity);
-                //        this.getServletContext().getRequestDispatcher("/test.jsp").forward(request, response);
-                //       
-                /* 
-                //////////////////code below its to fill up automaticly the food DB from text files
-         int count=0;
-         String fromfile="@@@";
-         File file = new File("C:\\Users\\Rock n Roll\\Desktop\\fatsToDb.txt");
-         Scanner in = new Scanner(file);
-         while (in.hasNextLine())
-         {
-         String line = in.nextLine();
-         String split[]=line.split(";");
-         //Vegetables v=new Vegetables();
-         // Fruits v=new Fruits();
-         // Starches v=new Starches();
-         // Meats v=new Meats();
-         // Beverages v=new Beverages();
-         Fatsandsweets v =new Fatsandsweets();
-         v.setFnsTitle(split[0]);
-         v.setFnsGrams(Integer.parseInt(split[1]));
-         v.setFnsMeasurements(split[2]);
-         v.setFnsCarbs(Integer.parseInt(split[3]));
-     
-         //foodHandlerBean.addFruits(v);
-         //foodHandlerBean.addStarches(v);
-         // foodHandlerBean.addMeats(v);
-         //foodHandlerBean.addBeverages(v);
-         foodHandlerBean.addFatsandsweets(v);
-         count++;
-         } 
-        
-        
-        
-    
-       
-        
-        
-        
-         //         List <Fruits> fruitsList;
-         //         fruitsList= foodHandlerBean.getAllFruits();
-        
-        
-         //  response.getWriter().write(fruitsList.get(3).getFruitTitle()+"e te tva e");
-         //JsonObject value = (JsonObject) Json.createObjectBuilder();
-                 */
+//       request.setAttribute("test", activity);
+//        this.getServletContext().getRequestDispatcher("/test.jsp").forward(request, response);
+//
+/*
+//////////////////code below its to fill up automaticly the food DB from text files
+int count=0;
+String fromfile="@@@";
+File file = new File("C:\\Users\\Rock n Roll\\Desktop\\fatsToDb.txt");
+Scanner in = new Scanner(file);
+while (in.hasNextLine())
+{
+String line = in.nextLine();
+String split[]=line.split(";");
+//Vegetables v=new Vegetables();
+// Fruits v=new Fruits();
+// Starches v=new Starches();
+// Meats v=new Meats();
+// Beverages v=new Beverages();
+Fatsandsweets v =new Fatsandsweets();
+v.setFnsTitle(split[0]);
+v.setFnsGrams(Integer.parseInt(split[1]));
+v.setFnsMeasurements(split[2]);
+v.setFnsCarbs(Integer.parseInt(split[3]));
+
+//foodHandlerBean.addFruits(v);
+//foodHandlerBean.addStarches(v);
+// foodHandlerBean.addMeats(v);
+//foodHandlerBean.addBeverages(v);
+foodHandlerBean.addFatsandsweets(v);
+count++;
+}
+
+
+
+
+
+
+
+
+//         List <Fruits> fruitsList;
+//         fruitsList= foodHandlerBean.getAllFruits();
+
+
+//  response.getWriter().write(fruitsList.get(3).getFruitTitle()+"e te tva e");
+//JsonObject value = (JsonObject) Json.createObjectBuilder();
+*/
+        } 
+        catch(ParseException ex)
+        {
+            Logger.getLogger(FoodHandler.class.getName()).log(Level.SEVERE, null, ex);
+
+        }      
     }
 
     @Override
--- mydiabetizer-war/src/java/statics/Diary.java
+++ mydiabetizer-war/src/java/statics/Diary.java
@@ -6,10 +6,22 @@
  package statics;
 
 import beans.diaryDatesBean;
+import ent.ChartEntity;
 import ent.Diarydates;
 import ent.TimeSlots;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
@@ -107,8 +119,136 @@ public class Diary
                     }
     }
        //  }
-   // }
+   // }List<List> timeList=new List<ArrayList>();
+     public ArrayList<ArrayList<ChartEntity>> timeList(List<ChartEntity> enteties) throws ParseException
+     {
+         
+         ArrayList<ArrayList<ChartEntity>> listOLists = new ArrayList<ArrayList<ChartEntity>>();
+         ArrayList<ChartEntity> sixToTwelve=new ArrayList<ChartEntity>();
+         ArrayList<ChartEntity> TwelveToEighteen=new ArrayList<ChartEntity>();
+         ArrayList<ChartEntity> EighteenToZero=new ArrayList<ChartEntity>();
+         ArrayList<ChartEntity> ZeroToSix=new ArrayList<ChartEntity>();
+         
+         
+         
+         String six = "06:00:00";
+         String twelve = "12:00:00";
+         String eighteen = "18:00:00";
+         String zero ="00:00:00";
+       //  String Compare ="13:56:01";        
 
+        DateFormat sdf = new SimpleDateFormat("hh:mm:ss");
+        Date dateSix = sdf.parse(six);
+        Date dateTwelve = sdf.parse(twelve);
+        Date dateEighteen = sdf.parse(eighteen);
+        Date dateZero = sdf.parse(zero);
+        //Date dateToCompare = sdf.parse(Compare);
+        
+         for(int i=0;i<enteties.size();i++)
+         {    
+               String Compare =enteties.get(i).getTime().trim();
+               Date dateToCompare = sdf.parse(Compare);
+           
+         if((dateToCompare.after(dateSix) && dateToCompare.before(dateTwelve)) || enteties.get(i).getDate().trim().equalsIgnoreCase(six) )
+         { 
+             sixToTwelve.add(enteties.get(i));
+         }
+         if((dateToCompare.after(dateTwelve) && dateToCompare.before(dateEighteen)) || enteties.get(i).getDate().trim().equalsIgnoreCase(twelve) )
+         { 
+             TwelveToEighteen.add(enteties.get(i));
+         }
+          if((dateToCompare.after(dateEighteen) && dateToCompare.before(dateZero)) || enteties.get(i).getDate().trim().equalsIgnoreCase(eighteen) )
+         { 
+             EighteenToZero.add(enteties.get(i));
+         }
+           if((dateToCompare.after(dateZero) && dateToCompare.before(dateSix)) || enteties.get(i).getDate().trim().equalsIgnoreCase(zero) )
+         { 
+             ZeroToSix.add(enteties.get(i));
+         }
+         }
+         listOLists.add(sixToTwelve);
+         listOLists.add(TwelveToEighteen);
+         listOLists.add(EighteenToZero);
+         listOLists.add(ZeroToSix);
+        //System.out.println("Time: " + sdf.format(date));
+         
+         
+        
+         
+        
+         
+         return listOLists;
+     }
+
+    
+    public JsonObject getJsonCustom(List<ChartEntity> enteties ) throws ParseException
+     {
+         String six = "06:00:00";
+         String twelve = "12:00:00";
+         String eighteen = "18:00:00";
+         String zero ="00:00:00";
+       //  String Compare ="13:56:01";        
+
+        DateFormat sdf = new SimpleDateFormat("hh:mm:ss");
+        Date dateSix = sdf.parse(six);
+        Date dateTwelve = sdf.parse(twelve);
+        Date dateEighteen = sdf.parse(eighteen);
+        Date dateZero = sdf.parse(zero);
+         
+         
+        JsonObjectBuilder jsonObjBuilder = Json.createObjectBuilder();
+        JsonArrayBuilder arrayBuilder1 = Json.createArrayBuilder();
+        JsonArrayBuilder arrayBuilder2 = Json.createArrayBuilder();
+        JsonArrayBuilder arrayBuilder3 = Json.createArrayBuilder();
+        JsonArrayBuilder arrayBuilder4 = Json.createArrayBuilder();
+        
+        JsonArray sixToTwelve;
+        JsonArray TwelveToEighteen;
+        JsonArray EighteenToZero;
+        JsonArray ZeroToSix;
+        String convertedDate="";
+        for(int i=0;i<enteties.size();i++)
+         {     
+               convertedDate=enteties.get(i).getDate().trim();
+               convertedDate.replaceAll("/", "-");
+               String Compare =enteties.get(i).getTime().trim();
+               Date dateToCompare = sdf.parse(Compare);
+           
+         if((dateToCompare.after(dateSix) && dateToCompare.before(dateTwelve)) || enteties.get(i).getDate().trim().equalsIgnoreCase(six) )
+         { 
+              
+              arrayBuilder1.add(jsonObjBuilder.add("date", enteties.get(i).getDate()).add("value", enteties.get(i).getSugar()));
+             // sixToTwelve.add(enteties.get(i));
+         }
+         if((dateToCompare.after(dateTwelve) && dateToCompare.before(dateEighteen)) || enteties.get(i).getDate().trim().equalsIgnoreCase(twelve) )
+         { 
+            arrayBuilder2.add(jsonObjBuilder.add("date", enteties.get(i).getDate()).add("value", enteties.get(i).getSugar()));
+         }
+          if((dateToCompare.after(dateEighteen) && dateToCompare.before(dateZero)) || enteties.get(i).getDate().trim().equalsIgnoreCase(eighteen) )
+         { 
+            arrayBuilder3.add(jsonObjBuilder.add("date", enteties.get(i).getDate()).add("value", enteties.get(i).getSugar()));
+         }
+           if((dateToCompare.after(dateZero) && dateToCompare.before(dateSix)) || enteties.get(i).getDate().trim().equalsIgnoreCase(zero) )
+         { 
+             arrayBuilder4.add(jsonObjBuilder.add("date", enteties.get(i).getDate()).add("value", enteties.get(i).getSugar()));
+         }
+         }
+          sixToTwelve=arrayBuilder1.build();
+          TwelveToEighteen=arrayBuilder2.build();
+          EighteenToZero=arrayBuilder3.build();
+          ZeroToSix=arrayBuilder4.build();
+       
+        
+      //  ja = arrayBuilder.build();
+        
+        JsonObject jsonObj = (JsonObject) jsonObjBuilder.add("sixToTwelve", sixToTwelve)
+                              .add("TwelveToEighteen", TwelveToEighteen)
+                              .add("EighteenToZero", EighteenToZero)
+                              .add("ZeroToSix", ZeroToSix);
+         
+         
+         return null;
+     }
     private diaryDatesBean lookupdiaryDatesBeanBean() {
         try {
             Context c = new InitialContext();
