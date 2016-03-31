84a0c55a6f2d1fb69b9831b32dd1248b2a713d7e
Rock n Roll <Rock n Roll@RocknRoll>
2016-03-31 11:32:55 +0100

    ....

---
+++ mydiabetizer-war/src/java/servlets/SessionCheker.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+/**
+ *
+ * @author Rock n Roll
+ */
+public class SessionCheker
+{
+    
+    
+    
+    
+    public boolean chekForSession()
+    {
+        
+        
+        
+        return false;
+    }
+    
+}
